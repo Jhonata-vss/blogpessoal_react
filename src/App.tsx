cafd70c93f83a71542ba4cdf19bb2c4e4ab79fd0
unknown <jhonatasenac15@gmail.com>
2024-08-26 12:16:54 -0300

    Criando novos componentes e estilizando com Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import Home from "./pages/home/Home"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
-  // Código TypeScript
-
+  //Código Typescript
   return (
-    // Código TSX => HTML + CSS
-    <> 
-
-      <Home />
-
-    </>
+  // Código TSX - HTML e CSS
+  <>
+    <BrowserRouter>
+        <Navbar/>
+        <div className="min-h-[80vh]">
+        <Routes>
+          <Route path = "/" element={<Home/>}/>
+          <Route path = "/home" element={<Home/>}/>
+        </Routes>
+        </div>
+        <Footer/>
+    </BrowserRouter>
+  </>
   )
 }
 
